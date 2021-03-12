06fa7d651f9afc5d1fced8ba0bb1e529563a1bd0
陈桥格 <qiaoge.chen@ikang.com>
2021-03-12 19:40:49 +0800

    ts add  nothing

--- dist/bundle.js
+++ dist/bundle.js
@@ -3,7 +3,7 @@
 
 	// TS中冒号后面的都为类型标识
 	// 1.基础类型
-	// : number string boolean 
+	// : number string boolean
 	var num1 = 1;
 	// ??? Number
 	var arr1 = [];
@@ -13,6 +13,10 @@
 	// 元组
 	var tuple = ['a', 1, false];
 	console.log(tuple[1]);
+	tuple.push('11');
+	// tuple[2] = 1 error
+	console.log(tuple); // ["a", 1, false, "11"]
+	// 枚举
 
 }());
 //# sourceMappingURL=bundle.js.map
